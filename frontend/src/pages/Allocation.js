c09e98dabfdd71044fbfa252adec5a61fde1f96c
vaishnavid7878-dev <vaishnavid7878@gmail.com>
2026-02-12 00:54:30 +0530

    Fixed ESLint error

--- frontend/src/pages/Allocation.js
+++ frontend/src/pages/Allocation.js
@@ -42,7 +42,6 @@ export default function Allocation() {
     fetchSubjects();
     fetchDivisions();
     fetchClassrooms();
-    fetchDivisions();
     fetchLabs();
     fetchAllocations();
   }, [token]);
